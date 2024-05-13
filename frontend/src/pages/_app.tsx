a02f3fee7aaa188658be53f7c9d3fb3edb32a307
HugoDvdTApp <hugo@toolapp.fr>
2024-05-13 09:40:38 +0200

    init

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+  // credentials: "include",
+});
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
+
+// Disabling SSR
+export default dynamic(() => Promise.resolve(App), { ssr: false });
